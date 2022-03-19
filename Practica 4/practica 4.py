0edf0e2bfe8cc434399af74166afc0ffddd1e16d
EpicMario <lara_mario2000@hotmail.com>
2022-03-19 11:26:25 -0600

    Practica 3 y 4 separadas

--- Practica 4/practica 4.py
+++ Practica 4/practica 4.py
@@ -19,27 +19,31 @@ def get_df() -> pd.DataFrame:
 
 df = get_df()
 filter_year = df["Date"].dt.year
+filter_name = df["Game_Name"]
 group_filter_year = df.groupby(filter_year)
 
-print("El juego con el mayor pico de jugadores por año: ")
-print(df.loc[group_filter_year["Peak_Players"].idxmax()][["Date", "Game_Name", "Peak_Players"]])
-
-print("El juego mayor perdida de jugadores por año: ")
-print(df.loc[group_filter_year["Gain"].idxmin()][["Date", "Game_Name", "Gain"]])
-
-print("El promedio de picos de jugadores por año")
-print(group_filter_year["Peak_Players"].mean().round(2))
-
 years = pd.unique(filter_year)
-game_names = df.loc[group_filter_year["Peak_Players"].idxmax()]["Game_Name"][::-1]
+top_game_names = df.loc[group_filter_year["Peak_Players"].idxmax()]["Game_Name"][::-1]
 
-for year, game_name in zip(years, game_names):
-    new_df = df[(filter_year == year) & (df["Game_Name"] == game_name)][["Date", "Peak_Players"]]
+for year, game_name in zip(years, top_game_names):
+    new_df = df[(filter_year == year) & (filter_name == game_name)][["Date", "Peak_Players"]]
     new_df = new_df.apply(lambda date: date.dt.month if date.name == "Date" else date)
     new_df.plot(x = "Date", 
                 y = "Peak_Players", 
-                kind = "bar", 
+                kind = "line", 
                 xlabel = year, 
                 title = game_name)
     plt.gca().ticklabel_format(axis='y', style='plain')
-    plt.savefig(f"practica 3/plots/peak_players_{year}.png")
+    plt.savefig(f"practica 4/peak_players_of_peak_games/peak_players_{year}.png")
+    plt.close()
+
+group_filter_name = df.groupby("Game_Name")
+new_df = df[(filter_name == game_name) & (filter_year == 2020)]
+new_df = group_filter_name["Peak_Players"].mean().round(2)
+new_df = new_df.head(10)
+new_df.plot(x = "Peak_Players", 
+            kind = "pie", 
+            ylabel = "",
+            title = "Top 10 mean of players of 2020")
+plt.gca().ticklabel_format(axis='y', style='plain')
+plt.savefig("practica 4/top_10_mean_players_of_2020.png")
